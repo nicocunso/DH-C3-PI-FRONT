b21fd9f98b0af4b5f3720d43b760363cf77a2f93
joseandrescuesta <andrescuesta@gmail.com>
2024-02-29 08:30:23 -0500

    Se creo entidad y endpoint para registrar usuario

---
+++ backend/src/main/java/com/carbook/backend/entities/Usuario.java
@@ -0,0 +1,23 @@
+package com.carbook.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String nombre;
+    @Column
+    private String apellido;
+    @Column
+    private String email;
+  //  private String password; --> pendiente hasta implementar Spring Security
+}
---
+++ backend/src/main/java/com/carbook/backend/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.carbook.backend.repository;
+
+import com.carbook.backend.entities.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
