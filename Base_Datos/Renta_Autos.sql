11eb25ddfdd242a6ad5350792600243935628691
Laurajuli <consultoriaderiesgosljg@gmail.com>
2024-03-04 22:13:30 -0500

    se modifica la base de datos en las claves primarias y foraneas

--- Base_Datos/Renta_Autos.sql
+++ Base_Datos/Renta_Autos.sql
@@ -14,7 +14,7 @@ CREATE database if not exists renta_autos;
 
 ### Crear tablas de renta de autos
 CREATE table if not exists renta_autos.clientes(
-id_cliente  int not null auto_increment, 
+id int not null auto_increment, 
 tipo_documento varchar (3) not null,
 numero_documento varchar (20) not null,
 nombres varchar (50) not null,
@@ -24,24 +24,24 @@ direccion varchar (50) not null,
 correo_electronico varchar (50)  not null,
 fecha_nacimiento date not null,
 creado timestamp not null,
-actualizado time not null,
+actualizado timestamp null,
 creado_por varchar (20) not null,
 actualizado_por varchar (20) not null,
-primary key (id_cliente)
+primary key (id)
 );
 
 CREATE table if not exists renta_autos.tipo_autos(
-id_tipo_auto int not null auto_increment,
+id int not null auto_increment,
 tipo_auto varchar (50) not null, 
 creado timestamp not null,
-actualizado time not null,
+actualizado timestamp not null,
 creado_por varchar (20) not null,
 actualizado_por varchar (20) not null,
-primary key (id_tipo_auto)
+primary key (id)
 );
 
 CREATE table if not exists renta_autos.autos(
-id_auto int not null auto_increment,
+id int not null auto_increment,
 id_tipo_auto int not null, 
 matricula varchar (50) not null, 
 marca varchar (50) not null,
@@ -55,93 +55,92 @@ nro_puertas int not null,
 nro_maleteros int not null, 
 nro_pasajeros int not null,
 creado timestamp not null,
-actualizado time not null,
+actualizado timestamp null,
 creado_por varchar (20) not null,
 actualizado_por varchar (20) not null,
-primary key (id_auto),
-foreign key (id_tipo_auto) references renta_autos.tipo_autos (id_tipo_auto)
+primary key (id),
+foreign key (id_tipo_auto) references renta_autos.tipo_autos (id)
 );
 
 CREATE table if not exists renta_autos.rentas(
-id_renta int not null auto_increment,
+id int not null auto_increment,
 id_cliente int not null, 
 id_auto int not null, 
 fecha_inicio timestamp not null,
 fecha_entrega timestamp not null,
 creado timestamp not null,
-actualizado time not null,
+actualizado timestamp null,
 creado_por varchar (20) not null,
 actualizado_por varchar (20) not null,
-primary key (id_renta),
-foreign key (id_cliente) references renta_autos.clientes (id_cliente),
-foreign key(id_auto) references renta_autos.autos (id_auto)
+primary key (id),
+foreign key (id_cliente) references renta_autos.clientes (id),
+foreign key(id_auto) references renta_autos.autos (id)
 );
 
 CREATE table if not exists renta_autos.facturas(
-id_factura int not null auto_increment,
+id int not null auto_increment,
 id_renta int not null, 
 valor_unitario decimal not null,
 valor_impuestos decimal null,
 valor_total decimal not null,
 fecha_factura timestamp not null,
 creado timestamp not null,
-actualizado time not null,
+actualizado timestamp null,
 creado_por varchar (20) not null,
 actualizado_por varchar (20) not null,
-primary key (id_factura),
-foreign key (id_renta) references renta_autos.rentas (id_renta)
+primary key (id),
+foreign key (id_renta) references renta_autos.rentas (id)
 );
 
 CREATE table if not exists renta_autos.pagos(
-id_pago int not null auto_increment,
+id int not null auto_increment,
 id_factura int not null, 
 valor_pago decimal not null,
 forma_pago varchar(20) not null,
 estado_pago varchar(20) not null,
 creado timestamp not null,
-actualizado time not null,
+actualizado timestamp null,
 creado_por varchar (20) not null,
 actualizado_por varchar (20) not null,
-primary key (id_pago),
-foreign key (id_factura) references renta_autos.facturas (id_factura)
+primary key (id),
+foreign key (id_factura) references renta_autos.facturas (id)
 );
 
 CREATE table if not exists renta_autos.tipo_usuarios(
-id_tipo_usuario int not null auto_increment,
+id int not null auto_increment,
 tipo_usuario varchar (20) not null, 
 descripcion varchar (50) null,
 creado timestamp not null,
-actualizado time not null,
+actualizado timestamp null,
 creado_por varchar (20) not null,
 actualizado_por varchar (20) not null,
-primary key (id_tipo_usuario)
+primary key (id)
 );
 
 CREATE table if not exists renta_autos.usuarios(
-id_usuario int not null auto_increment,
+id int not null auto_increment,
 id_tipo_usuario int not null, 
 usuario varchar (20) null,
 contrasena varchar (20) null,
-actualizado time not null,
+creado timestamp not null,
+actualizado timestamp null,
 creado_por varchar (20) not null,
 actualizado_por varchar (20) not null,
-primary key (id_usuario),
-foreign key (id_tipo_usuario) references renta_autos.tipo_usuarios (id_tipo_usuario)
+primary key (id),
+foreign key (id_tipo_usuario) references renta_autos.tipo_usuarios (id)
 );
 
 CREATE table if not exists renta_autos.imagenes(
-id_imagen int not null auto_increment, 
+id int not null auto_increment, 
 id_auto int not null, 
 url varchar (400) not null,
 creado timestamp not null,
-actualizado time not null,
+actualizado timestamp not null,
 creado_por varchar (20) not null,
 actualizado_por varchar (20) not null,
-primary key (id_imagen),
-foreign key (id_auto) references renta_autos.autos (id_auto)
+primary key (id),
+foreign key (id_auto) references renta_autos.autos (id)
 ); 
-
-### Insertar Registros tablas
 
 
 
