4ae0616025b26344af46f028e18de089f365aa66
nicolas <kornico94@gmail.com>
2024-03-15 19:47:50 -0300

    arreglando conflictos v1

--- Proyecto final/ProyectoIntegrador/src/Components/Recomendaciones/Recomendaciones.jsx
+++ Proyecto final/ProyectoIntegrador/src/Components/Recomendaciones/Recomendaciones.jsx
@@ -6,61 +6,6 @@ import Imagenes from '../Imagenes.jsx/Imagenes';
 
 const Recomendaciones = () => {
   const [mostrarDetalle, setMostrarDetalle] = useState(false);
-  const [recomendacionSeleccionada, setRecomendacionSeleccionada] = useState(null);
-  const [recomendaciones, setRecomendaciones] = useState([]);
-  const [paginaActual, setPaginaActual] = useState(1);
-  const recomendacionesPorPagina = 10;
-
-  // Hook al montar el componente
-  useEffect(() => {
-    // Efecto secundario para obtener los autos
-    obtenerRecomendaciones();
-    // Efecto secundario para barajar las recomendaciones
-    barajarRecomendaciones();
-  }, []);
-
-  const recomendacionesMock = [
-    { id: 1, modelo: 'Corolla', anno: 2022, aireAcondicionado: 'Aire acondicionado', imagenes: Imagenes.Toyota },
-    { id: 2, modelo: 'Civic', anno: 2021, aireAcondicionado: 'Aire acondicionado', imagenes: Imagenes.Honda },
-    { id: 3, modelo: 'Mustang', anno: 2023, aireAcondicionado: 'Aire acondicionado', imagenes: Imagenes.Ford },
-    { id: 4, modelo: 'Camaro', anno: 2020, aireAcondicionado: 'Aire acondicionado', imagenes: Imagenes.Chevrolet },
-    { id: 5, modelo: 'Golf', anno: 2022, aireAcondicionado: 'Aire acondicionado', imagenes: Imagenes.Golf },
-    { id: 6, modelo: 'Serie 3', anno: 2021, aireAcondicionado: 'Aire acondicionado', imagenes: Imagenes.BMW },
-    { id: 7, modelo: 'Clase C', anno: 2024, aireAcondicionado: 'Aire acondicionado', imagenes: Imagenes.Mercedes },
-    { id: 8, modelo: 'A4', anno: 2020, aireAcondicionado: 'Aire acondicionado', imagenes: Imagenes.Audi },
-    { id: 9, modelo: 'Altima', anno: 2023, aireAcondicionado: 'Aire acondicionado', imagenes: Imagenes.Nissan },
-    { id: 10, modelo: 'Elantra', anno: 2022, aireAcondicionado: 'Aire acondicionado', imagenes: Imagenes.Hyundai },
-    { id: 11, modelo: 'Elantra', anno: 2022, aireAcondicionado: 'Aire acondicionado', imagenes: Imagenes.Hyundai },
-  ];
-
-  // Función para obtener los autos
-  const obtenerRecomendaciones = () => {
-    fetch('http://localhost:8080/autos')
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        if (data) {
-          setRecomendaciones(data);
-        } else {
-          setRecomendaciones(recomendacionesMock);
-        }
-      });
-  }
-
-  // Función para barajar las recomendaciones de forma aleatoria
-  const barajarRecomendaciones = () => {
-    const recomendacionesBarajadas = [...recomendaciones];
-    for (let i = recomendacionesBarajadas.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [recomendacionesBarajadas[i], recomendacionesBarajadas[j]] = [
-        recomendacionesBarajadas[j],
-        recomendacionesBarajadas[i],
-      ];
-    }
-    setRecomendaciones(recomendacionesBarajadas);
-  };
   const [recomendacionSeleccionada, setRecomendacionSeleccionada] = useState(null); 
 
   const toggleDetalle = (recomendacion) => { 
