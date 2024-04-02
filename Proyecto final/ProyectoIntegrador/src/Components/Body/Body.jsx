27ad3b80a9f5a6b559d9fe95296930c1aa17db53
Laura <consultoriaderiesgosljg@gmail.com>
2024-04-02 19:56:46 -0300

    Agregando boton de whatsapp con redireccionamiento.

--- Proyecto final/ProyectoIntegrador/src/Components/Body/Body.jsx
+++ Proyecto final/ProyectoIntegrador/src/Components/Body/Body.jsx
@@ -1,35 +1,40 @@
-import React, { useState } from 'react';
-import bodyStyles from './Body.module.css'; 
-import Buscador from '../Buscador/Buscador';
-import Categorias from '../Categorias/Categorias';
-import Footer from '../Footer/Footer'
-import Recomendaciones from '../Recomendaciones/Recomendaciones';
-import Header from '../Header/Header';
-import Resultados from '../Resultados/Resultados';
+import React, { useState } from "react";
+import bodyStyles from "./Body.module.css";
+import Buscador from "../Buscador/Buscador";
+import Categorias from "../Categorias/Categorias";
+import Footer from "../Footer/Footer";
+import Recomendaciones from "../Recomendaciones/Recomendaciones";
+import Header from "../Header/Header";
+import Resultados from "../Resultados/Resultados";
+import Whatsapp from "../whatsapp/Whatsapp";
 
 const Body = () => {
-  const [modeloFiltrado, setModeloFiltrado] = useState('');
+  const [modeloFiltrado, setModeloFiltrado] = useState("");
 
-  const buscarAutosPorModelo = modelo => {
+  const buscarAutosPorModelo = (modelo) => {
     setModeloFiltrado(modelo);
   };
 
-  const handleEnterPress = event => {
-    if (event.key === 'Enter') {
+  const handleEnterPress = (event) => {
+    if (event.key === "Enter") {
       buscarAutosPorModelo(event.target.value);
     }
   };
 
   return (
     <div className={bodyStyles.body}>
-      <Header/>
-      <Buscador onBuscar={buscarAutosPorModelo} onEnterPress={handleEnterPress} />
-      <Categorias/>
-      <Recomendaciones/>
+      <Header />
+      <Buscador
+        onBuscar={buscarAutosPorModelo}
+        onEnterPress={handleEnterPress}
+      />
+      <Categorias />
+      <Recomendaciones />
+      <Whatsapp />
       <Resultados modeloFiltrado={modeloFiltrado} />
-      <Footer/>
+      <Footer />
     </div>
   );
-}
+};
 
 export default Body;
