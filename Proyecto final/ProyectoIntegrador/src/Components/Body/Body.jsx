6d7c41114ae19276b7eedba58d80db29e825e2cb
Laura <consultoriaderiesgosljg@gmail.com>
2024-04-02 20:15:33 -0300

    Agregando boton de Whatsapp con redireccionamiento.

--- Proyecto final/ProyectoIntegrador/src/Components/Body/Body.jsx
+++ Proyecto final/ProyectoIntegrador/src/Components/Body/Body.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import bodyStyles from './Body.module.css'; 
-import Buscador from '../Buscador/Buscador';
-import Categorias from '../Categorias/Categorias';
-import Footer from '../Footer/Footer'
-import Recomendaciones from '../Recomendaciones/Recomendaciones';
-import Header from '../Header/Header';
+import React from "react";
+import bodyStyles from "./Body.module.css";
+import Buscador from "../Buscador/Buscador";
+import Categorias from "../Categorias/Categorias";
+import Footer from "../Footer/Footer";
+import Recomendaciones from "../Recomendaciones/Recomendaciones";
+import Header from "../Header/Header";
+import Whatsapp from "../whatsapp/Whatsapp";
 
 const Body = () => {
   return (
     <div className={bodyStyles.body}>
-      <Header/>
-      <Buscador/>
-      <Categorias/>
-      <Recomendaciones/>
-      <Footer/>
+      <Header />
+      <Buscador />
+      <Categorias />
+      <Whatsapp />
+      <Recomendaciones />
+      <Footer />
     </div>
   );
-}
+};
 
 export default Body;
